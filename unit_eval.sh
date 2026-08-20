63ddf1a944c6ad62ce53ce60edb52e1cc24f659c
LizhengMathAi <52631612+LizhengMathAi@users.noreply.github.com>
2026-08-20 16:03:20 -0400

    Add files via upload

--- unit_eval.sh
+++ unit_eval.sh
@@ -1,4 +1,4 @@
-python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.0
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.0
 # python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.1
 # python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.2
 # python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.3
@@ -23,3 +23,30 @@ python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.0
 # python unit_eval.py --root-dir ./acdc_test --mode baseline --thres 0.9
 # python unit_eval.py --root-dir ./acdc_test --mode self_derived_prompts
 # python unit_eval.py --root-dir ./acdc_test --mode annotation_derived_prompts
+
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.0 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.1 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.2 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.3 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.4 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.5 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.6 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.7 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.8 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.9 --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode self_derived_prompts --operation areas --patient-id 1
+# python unit_eval.py --root-dir ./acdc_train --mode annotation_derived_prompts --operation areas --patient-id 1
+
+
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.0 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.1 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.2 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.3 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.4 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.5 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.6 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.7 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.8 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode baseline --thres 0.9 --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode self_derived_prompts --operation neighbor_dice --patient-id 1
+python unit_eval.py --root-dir ./acdc_train --mode annotation_derived_prompts --operation neighbor_dice --patient-id 1
